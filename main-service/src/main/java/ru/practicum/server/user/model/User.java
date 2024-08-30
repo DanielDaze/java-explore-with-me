311536da1a517081cf3787fdef2c85c5dec87309
DanielDaze <tattoo255@yandex.ru>
2024-08-30 18:29:47 +0300

    Public Category Logic DONE + SQLIntegrity and NotFound handling

--- main-service/src/main/java/ru/practicum/server/user/model/User.java
+++ main-service/src/main/java/ru/practicum/server/user/model/User.java
@@ -1,15 +1,13 @@
 package ru.practicum.server.user.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 @Entity
 @Table(name = "users")
 public class User {
